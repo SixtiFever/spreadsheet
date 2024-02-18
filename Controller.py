8b1993f6c4a080f01589f0452b7c3f7e3a72102e
Jason <jdswift94@gmail.com>
2024-02-18 10:56:57 +0000

    if cell formula is a reference, operations are correctly being carried out. Except unary subtraction, which I will go back to implement at some point

--- Controller.py
+++ Controller.py
@@ -1,5 +1,8 @@
 import sqlite3
 from flask import g
+import operator
+
+operator_dict = {'+': operator.add, '-': operator.sub, 'x': operator.mul, '/': operator.truediv}
 
 # initialises database and creates table
 def init_db():
@@ -19,6 +22,34 @@ def is_numeric_string(formula):
         # If ValueError occurs, it means the string cannot be cast to an integer
         return False
     
+def perform_reference_ops(formula, cursor):
+    """
+    performs operations when a cell references another cell
+    """
+    # is a reference -> Perform operations
+    operands = formula.split(' ')
+    # if single reference E.g D4 -> Get and print value
+    if len(operands) <= 1:
+        res = cursor.execute("SELECT * FROM cells WHERE id='" + operands[0] + "'")
+        val = res.fetchall()[0][1] # obtain calue of cell id
+        print(id + ': '+ val)
+    else:
+        print(operands)
+        # for each reference in operands, obtain the value and replace in-place
+        sum = float(cursor.execute("SELECT * FROM cells WHERE id='" + operands[0] + "'").fetchall()[0][1]) if len(cursor.execute("SELECT * FROM cells WHERE id='" + operands[0] + "'").fetchall()) >= 1 else 0 
+        o = 1
+        r = 2
+        for i in range(0, len(operands)):
+            if r > len(operands):
+                        break
+            right_val = float(cursor.execute("SELECT * FROM cells WHERE id='" + operands[r] + "'").fetchall()[0][1]) if len(cursor.execute("SELECT * FROM cells WHERE id='" + operands[r] + "'").fetchall()) >= 1 else 0 
+            print(right_val)
+            op = operator_dict[operands[o]]
+            sum = op(sum,right_val)
+            o += 2
+            r += 2
+        print(sum)
+    
 
 
 
@@ -36,12 +67,16 @@ def create(id, formula = 0):
         cursor.execute("SELECT * FROM cells WHERE id=?", (id,))
         if len(cursor.fetchall()) <= 0:
             # insert new cell
+            print('Inserted new cell ' + id)
             cursor.execute("INSERT INTO cells VALUES (?, ?)", (id, formula))
         else:
-            print('Cell ' + id + ' already exists')
+            # update cell
+            print('Updated cell ' + id)
+            cursor.execute("UPDATE cells SET formula='"+formula+"' WHERE id='" + id + "'")
 
         conn.execute("COMMIT")
     except:
+        # rollback to previous version if error
         conn.execute("ROLLBACK")
         raise
     finally:
@@ -51,6 +86,7 @@ def create(id, formula = 0):
 
 
 def read(id = 0):
+
     conn = sqlite3.connect('sheet.db')
     cursor = conn.cursor()
 
@@ -58,22 +94,30 @@ def read(id = 0):
         conn.execute("BEGIN")
         
         if id == 0:
-            # read all cells
+            # list all cells
             cursor.execute("SELECT * FROM cells")
             print(cursor.fetchall())
+
         else:
             # read 1 cell
             cursor.execute("SELECT * FROM cells WHERE id=?", (id,))
             result = cursor.fetchall()
+            # if cell doesn't exist, print 0 and return
+            if len(result) <= 0:
+                print(0)
+                return
+            
             formula = result[0][1]
 
             # check whether cell is value or a reference to other cell(s)
             if is_numeric_string(formula):
                 # is numeric -> Read value
                 print(formula)
+             
+
             else:
-                # is a reference -> Perform operations
-                print('ref')
+                # is a reference -> perform operations on the formula
+                perform_reference_ops(formula, cursor)
 
         conn.execute("COMMIT")
     except:
@@ -93,6 +137,6 @@ def delete(id):
     return ""
 
 
-create('D5', '33.7')
-read('A1')
+create('D6', 'A1 + A2 - A3 x A4 / A1')
+read('D6')
 
